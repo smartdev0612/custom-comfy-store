61247665ab08ac6cf6034ebdc45413e143bc1cf2
Smart Dev <danieldev0612@gmail.com>
2023-02-15 18:55:18 -0800

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import Home from './HomePage'
+import About from './AboutPage'
+import Cart from './CartPage'
+import Checkout from './CheckoutPage'
+import Error from './ErrorPage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import AuthWrapper from './AuthWrapper'
+import PrivateRoute from './PrivateRoute'
+
+export {
+  Home,
+  About,
+  Cart,
+  Checkout,
+  Error,
+  Products,
+  SingleProduct,
+  AuthWrapper,
+  PrivateRoute,
+}
